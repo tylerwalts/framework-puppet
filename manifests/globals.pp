7a0ce80d52fd68772461e602c35481fa437591ba
Tyler Walters <github@tylerwalts.com>
2013-08-28 08:25:23 -0500

    Fixed broken default value, changed to assume vagrant path by default for hiera location

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -3,14 +3,10 @@ Exec { path        => [ '/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbi
 
 # Hiera needs to know where to find the config, relative to puppet root
 if ( $ec2_security_groups == "elasticbamboo" ) {
-    # Bamboo
+    # VM launched by Bamboo
     $puppet_cwd="/mnt/bamboo-ebs/bin/code/tools/puppet/"
-} elsif ( $::fqdn =~ /vagrant/ ) {
-    # Vagrant
-    $puppet_cwd="/vagrant/tools/puppet/"
 } else {
-    # Default
-    $puppet_cwd="."
+    # Default assumes VM launched by Vagrant
+    $puppet_cwd="/vagrant/tools/puppet/"
 }
-
 
