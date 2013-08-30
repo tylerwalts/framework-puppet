f1f474d5637e78d97c128c59bb00c16c65993f89
Tyler Walters <github@tylerwalts.com>
2013-08-30 08:11:41 -0500

    Fixed regex typo

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -2,7 +2,7 @@
 Exec { path        => [ '/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin', ], }
 
 # Hiera needs to know where to find the config, relative to puppet root
-if ( $ec2_security_groups =~ "elasticbamboo" ) {
+if ( $ec2_security_groups =~ /elasticbamboo/ ) {
     # VM launched by Bamboo
     $puppet_cwd="/mnt/bamboo-ebs/bin/code/tools/puppet/"
 } else {
