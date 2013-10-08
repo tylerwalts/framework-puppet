7b772f70d46495b8e7d0d3a402ecfa176b687f79
Tyler Walters <github@tylerwalts.com>
2013-10-08 15:21:52 -0500

    Added noop flag and updated usage prompt

--- run_puppet_apply.sh
+++ run_puppet_apply.sh
@@ -4,7 +4,14 @@ basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 function usagePrompt {
-    echo -e "\n\trun_puppet_apply.sh [options]\n\n\t-l | --librarian\t\tRun Puppet Librarian to update modules"
+    echo -e "
+    run_puppet_apply.sh [options]
+
+    -l | --librarian      Run Puppet Librarian to update modules
+    -f | --facter         Run Puppet and use custom facter overrides
+    -n | --noop           Run Puppet in No Operation (noop) mode
+
+    "
 }
 
 while [ "$1" != "" ]; do
@@ -21,8 +28,15 @@ while [ "$1" != "" ]; do
             export $facterOptions
             shift
             ;;
+        -n | --noop )
+            shift
+            noopArg=" --noop "
+            echo "Using noop (no operation) run mode - no changes will be realized"
+            shift
+            ;;
         *) usagePrompt
             shift
+            exit
             ;;
     esac
 done
@@ -36,6 +50,6 @@ echo "Running puppet version: $puppetVersion"
 export FACTER_hiera_config="$basedir/manifests/config"
 
 cd ${basedir}/manifests
-command="puppet apply $puppetOpts --modulepath ${basedir}/modules/:${basedir}/lib/ ${basedir}/manifests/site.pp"
+command="puppet apply $puppetOpts $noopArg --modulepath ${basedir}/modules/:${basedir}/lib/ ${basedir}/manifests/site.pp"
 echo -e "\nRunning Masterless Puppet using command: \n\t$command\n"
 $command
