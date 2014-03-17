9041feb30e1f44d283359a0094df6672c53da044
Tyler Walters <git@tylerwalts.com>
2014-03-16 21:02:27 -0500

    #13 - deprecated use of import statements, moved .pp files into single site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,38 @@
 # Include global puppet settings
-import "globals.pp"
+### Globals ###
+# Global default
+Exec { path        => [ '/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin', ], }
 
-# Include project-level global puppet settings, if any
-import "globals-project.pp"
+# By convention, can control environments using multiple levels of subdomains.
+# 7 levels should be enough for now.
+# For example, with domain:  web.test.example.com
+# 1st-level-domain:  com
+# 2nd-level-domain:  example
+# 3rd-level-domain:  test (aka "subdomain")
+# 4rd-level-domain:  web (aka "hostname")
+# 5th-level-domain:  -- (empty)
+# 6th-level-domain:  -- (empty)
+# 7th-level-domain:  -- (empty)
+#
+# These are used mapped in hiera.yaml to find the corresponding config in the subdirs
+#
+$domain_levels = split($domain, '[.]')
+if (size($domain_levels)>0) { $domain1 = $domain_levels[size($domain_levels)-1] }
+if (size($domain_levels)>1) { $domain2 = $domain_levels[size($domain_levels)-2] }
+if (size($domain_levels)>2) { $domain3 = $domain_levels[size($domain_levels)-3] }
+if (size($domain_levels)>3) { $domain4 = $domain_levels[size($domain_levels)-4] }
+if (size($domain_levels)>4) { $domain5 = $domain_levels[size($domain_levels)-5] }
+if (size($domain_levels)>5) { $domain6 = $domain_levels[size($domain_levels)-6] }
+if (size($domain_levels)>6) { $domain7 = $domain_levels[size($domain_levels)-7] }
 
+
+### Classes ###
 # Assign Classes, Defines & Parameters using Hiera
 hiera_include("classes")
 
-# Realize Defines, if any
-import "defines.pp"
+
+### Defines ###
+# Create a pair of lookups and creations for each define:
+# Example:
+#create_resources('moduleName::define', hiera('example_defines'))
 
